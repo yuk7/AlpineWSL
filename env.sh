2b22b84856279adf221ddd37dd92ec6da901623d
yuk7 <yukx00@gmail.com>
2018-08-21 14:27:32 +0900

    env.sh: sort downloader priority

--- env.sh
+++ env.sh
@@ -16,11 +16,11 @@ LNCR_FN="Alpine.exe"
 LNCR_URL="https://github.com/yuk7/wsldl/releases/download/${LNCR_BLD}/${LNCR_ZIP}"
 
 
-if type curl >/dev/null 2>&1 ;then
-    DLR="curl"
-fi
 if type wget >/dev/null 2>&1;then
     DLR="wget"
+fi
+if type curl >/dev/null 2>&1 ;then
+    DLR="curl"
 fi
 if type aria2c >/dev/null 2>&1; then
     DLR="aria2c -x4"
